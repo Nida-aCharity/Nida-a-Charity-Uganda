ba3cdf4bcad06195652c146b0c045099a8be90c0
Nida-aCharity <nidaacharity@gmail.com>
2025-06-26 11:18:12 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	This website is managed by Mwima Amiwar, for verification on other service providers like 
+payoneer, and other platforms that may require verification. contact: nadaacharity@gmail.com
 */
 
 (function($) {
